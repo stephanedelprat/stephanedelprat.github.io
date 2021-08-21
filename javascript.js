4815e6e52c61308bba7cf39d7dd9a3ec32ddc3fe
Stéphane <stephane@holaspirit.com>
2021-08-21 16:51:28 +0900

    Add page 2

--- javascript.js
+++ javascript.js
@@ -7,6 +7,11 @@ init = function() {
         let binary = number.toString(2);
         question.innerHTML = binary + '<br><span class="response hidden-response">Réponse: ' + number + '</span>';
     });
+    Array.prototype.filter.call(document.getElementsByClassName('question2'), function (question) {
+        let number = 56 + random(200);
+        let binary = number.toString(2);
+        question.innerHTML = number + '<br><span class="response hidden-response">Réponse: ' + binary + '</span>';
+    });
 }
 init();
 document.getElementById('show').addEventListener("click", function() {
