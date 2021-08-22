33bf0f7304554d483610186ded94370b644e57e3
Stéphane <stephane@holaspirit.com>
2021-08-22 16:44:47 +0900

    Addition

--- javascript.js
+++ javascript.js
@@ -1,16 +1,26 @@
-random = function(size) {
-    return Math.floor(Math.random()*size);
+random = function(start, end) {
+    return start + Math.floor(Math.random() * (end - start + 1));
 }
 init = function() {
-    Array.prototype.filter.call(document.getElementsByClassName('question1'), function (question) {
-        let number = 56 + random(200);
+    Array.prototype.filter.call(document.querySelectorAll('body.base2-10 div.question'), function (question) {
+        let number = random(48, 255);
         let binary = number.toString(2);
         question.innerHTML = binary + '<br><span class="response hidden-response">Réponse: ' + number + '</span>';
     });
-    Array.prototype.filter.call(document.getElementsByClassName('question2'), function (question) {
-        let number = 56 + random(200);
+    Array.prototype.filter.call(document.querySelectorAll('body.base10-2 div.question'), function (question) {
+        let number = random(48, 255);
         let binary = number.toString(2);
         question.innerHTML = number + '<br><span class="response hidden-response">Réponse: ' + binary + '</span>';
+    });
+    Array.prototype.filter.call(document.querySelectorAll('body.base2-add div.question'), function (question) {
+        let number1, number2;
+        do {
+            number1 = random(10, 245);
+            number2 = random(10, 245);
+        } while (number1 + number2 > 255)
+        let binary1 = number1.toString(2);
+        let binary2 = number2.toString(2);
+        question.innerHTML = binary1 + ' + ' + binary2 + '<br><span class="response hidden-response">Réponse: ' + (number1 + number2).toString(2) + '</span>';
     });
 }
 init();
